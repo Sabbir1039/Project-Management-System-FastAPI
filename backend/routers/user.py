05c086e7e4ff02f5a742b4e41ecaa854a042657d
Sabbir Hossain <cse.170201039@gmail.com>
2024-07-06 00:24:04 +0600

    update_user method added

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from typing import List
-from ..schemas.user import UserCreateSchema, UserResponseSchema
+from ..schemas.user import UserCreateSchema, UserResponseSchema, UserUpdateSchema
 from ..database import get_db
 from ..crud.user import create_user, get_user, get_user_by_email, get_users
+from ..utilities import get_hashed_password
 
 
 router = APIRouter()
@@ -14,6 +15,25 @@ def register_user(user: UserCreateSchema, db: Session = Depends(get_db)):
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return create_user(db=db, user=user)
+
+@router.put("/update/{user_id}", response_model=UserResponseSchema)
+def update_user(user_id: int, user: UserUpdateSchema, db: Session = Depends(get_db)):
+    try:
+        db_user = get_user(db, user_id)
+        if db_user:
+            if user.username:
+                db_user.username = user.username
+            if user.email:
+                db_user.email = user.email
+            if user.password:
+                db_user.hashed_password = get_hashed_password(user.password)
+            db.commit()
+            db.refresh(db_user)
+            return db_user
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
 @router.get("/{user_id}", response_model=UserResponseSchema)
