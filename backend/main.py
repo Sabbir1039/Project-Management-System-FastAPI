4613427caf6d05f835dbf655fd64525e2cd817c4
Sabbir Hossain <cse.170201039@gmail.com>
2024-07-06 14:05:07 +0600

    cors middleware added for frontend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import user, project, task, auth
 from .database import engine, Base
 from .models import user as user_model, project as project_model, task as task_model
@@ -6,6 +7,21 @@ from .models import user as user_model, project as project_model, task as task_m
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    # "http://your-frontend-domain.com", 
+]
+
+# CORSMiddleware to the FastAPI app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow requests from these origins
+    allow_credentials=True,  # Allow cookies to be sent with requests
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(user.router, prefix="/users", tags=["users"])
 app.include_router(project.router, prefix="/projects", tags=["projects"])
